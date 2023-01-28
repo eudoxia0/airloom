5c7d2a816fe01870993e09370bd1341955446a47
Fernando Borretti <fernando@borretti.me>
2023-01-28 20:06:52 +1100

    Types for lines

--- src/AirLoom/Parser.hs
+++ src/AirLoom/Parser.hs
@@ -7,6 +7,14 @@ data SourceTag = FragmentStartTag String
 -- The types of tags we can encounter in documentation files.
 data DocTag = TranscludeTag String
 
+-- The type of lines in source files.
+data SourceLine = SourceTextLine String
+                | SourceTagLine SourceTag
+
+-- The type of lines in documentation files.
+data DocLine = DocTextLine String
+             | DocTagLine DocTag
+
 loomStartRegex :: String
 loomStartRegex = "[^\\\\]loom:start\\(([a-zA-Z0-9,':]+)\\)"
 
