dfb6fac8f090ea5fc56eb09ae2043ca3c5c759bd
Fernando Borretti <fernando@borretti.me>
2023-01-28 18:19:36 +1100

    Rename main module

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import AirLoom (someFunc)
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  someFunc
--- src/AirLoom.hs
+++ src/AirLoom.hs
@@ -1,4 +1,4 @@
-module MyLib (someFunc) where
+module AirLoom (someFunc) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
