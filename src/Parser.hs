26855ef6bb9ab6c2280162fbddada97764a417c2
Fernando Borretti <fernando@borretti.me>
2023-01-28 19:12:11 +1100

    Parse lines containing tags

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,11 +1,19 @@
 module Parser where
-import Text.Parsec (many1, (<|>))
+import Text.Parsec (many1, manyTill, (<|>))
 import Text.Parsec.String (Parser)
-import Text.Parsec.Char (string, char, alphaNum, oneOf)
+import Text.Parsec.Char (string, char, anyChar, alphaNum, oneOf, newline)
 
 -- The types of tags we can encounter in source files.
 data SourceTag = FragmentStartTag String
                | FragmentEndTag String
+
+fragmentStartLine :: Parser SourceTag
+fragmentStartLine = do
+  _ <- manyTill anyChar (string "loom:start")
+  tag <- fragmentStartTag
+  _ <- manyTill anyChar newline
+  return tag
+
 
 fragmentStartTag :: Parser SourceTag
 fragmentStartTag = do
