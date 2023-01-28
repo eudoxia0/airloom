b75f6ac86de2d5680c4bfdcc268d7d096c57eaf3
Fernando Borretti <fernando@borretti.me>
2023-01-28 20:28:38 +1100

    experts, derive eq and show, implement parseSourceLine

--- src/AirLoom/Parser.hs
+++ src/AirLoom/Parser.hs
@@ -1,28 +1,41 @@
-module AirLoom.Parser where 
+module AirLoom.Parser (
+  SourceTag (..),
+  DocTag (..),
+  SourceLine (..),
+  DocLine (..),
+  parseSourceLine  
+) where 
+import Text.Regex.TDFA
 
 -- The types of tags we can encounter in source files.
 data SourceTag = FragmentStartTag String
                | FragmentEndTag String
+  deriving (Eq, Show)
 
 -- The types of tags we can encounter in documentation files.
 data DocTag = TranscludeTag String
+  deriving (Eq, Show)
 
 -- The type of lines in source files: either a source line or a tag like
 -- `loom:start` or `loom:end`.
 data SourceLine = SourceTextLine String
                 | SourceTagLine SourceTag
+  deriving (Eq, Show)
 
 -- The type of lines in documentation files: either a text line or a tag like
 -- `loom:include`.
 data DocLine = DocTextLine String
              | DocTagLine DocTag
+  deriving (Eq, Show)
 
 -- Given a line of text, returns a `SourceTagLine` if there's a `loom:*` tag,
 -- or a `SourceTextLine` otherwise.
 parseSourceLine :: String -> SourceLine
 parseSourceLine line =
-  ""
-  
+  case line =~ loomStartRegex :: (String, String, String, [String]) of
+      (_, _, _, text : []) -> SourceTagLine $ FragmentStartTag text
+      _                    -> SourceTextLine line
+
 -- Matches `loom:start` tags.
 loomStartRegex :: String
 loomStartRegex = tagRegex "loom:start"
@@ -38,4 +51,4 @@ loomIncludeRegex = tagRegex "loom:include"
 -- Makes a regular expression for matching a tag with the given name.
 tagRegex :: String -> String
 tagRegex s =
-  "[^\\\\]" ++ s ++ "\\(([a-zA-Z0-9,':]+)\\)"
+  "^.*[^\\\\]" ++ s ++ "\\(([a-zA-Z0-9,':]+)\\).*$"
