04eddc9489839ec814f3fb366209ec269631d061
Fernando Borretti <fernando@borretti.me>
2023-01-28 20:05:22 +1100

    Replace the parser with regexes

--- src/AirLoom/Parser.hs
+++ src/AirLoom/Parser.hs
@@ -1,41 +1,14 @@
-module AirLoom.Parser (loomStart, fragmentStartLine, fragmentStartTag, fragmentName, nameChar, SourceTag (FragmentStartTag, FragmentEndTag))
-where
-import Text.Parsec (many1, manyTill, (<|>))
-import Text.Parsec.String (Parser)
-import Text.Parsec.Char (string, char, anyChar, alphaNum, oneOf, noneOf, newline)
+module AirLoom.Parser where
 
 -- The types of tags we can encounter in source files.
 data SourceTag = FragmentStartTag String
                | FragmentEndTag String
 
--- Parses 'loom:start', unless prefixed by a backslash.
-loomStart :: Parser String
-loomStart = do
-  _ <- noneOf "\\"
-  string "loom:start"
+loomStartRegex :: String
+loomStartRegex = "[^\\\\]loom:start\\(([a-zA-Z0-9,':]+)\\)"
 
--- Parses a line containing a `loom:start` tag, and returns the name.
-fragmentStartLine :: Parser SourceTag
-fragmentStartLine = do
-  _ <- manyTill anyChar loomStart
-  tag <- fragmentStartTag
-  _ <- manyTill anyChar newline
-  return tag
+loomEndRegex :: String
+loomEndRegex = "[^\\\\]loom:start\\(([a-zA-Z0-9,':]+)\\)"
 
--- Parses the contents of a `loom:start` tag.
-fragmentStartTag :: Parser SourceTag
-fragmentStartTag = do
-  _ <- char '('
-  name <- fragmentName
-  _ <- char ')'
-  return (FragmentStartTag name)
-
--- Parses the name of a fragment.
-fragmentName :: Parser String
-fragmentName =
-  many1 nameChar
-
--- Valid characters for fragment names.
-nameChar :: Parser Char
-nameChar =
-  alphaNum <|> oneOf " ,':"
+-- The types of tags we can encounter in documentation files.
+data Docag = TranscludeTag String
