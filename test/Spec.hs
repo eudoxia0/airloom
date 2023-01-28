af3987acd27e69e60c255ab4bfcc06ae72123955
Fernando Borretti <fernando@borretti.me>
2023-01-28 20:46:16 +1100

    reorder tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,20 @@
 module Main (main) where
 import Test.HUnit
-import AirLoom.Parser (parseSourceLine, SourceLine (SourceTextLine))
+import AirLoom.Parser (parseSourceLine, SourceLine (SourceTextLine, SourceTagLine), SourceTag (FragmentStartTag, FragmentEndTag))
 
 -- Parser tests.
 
 parseSourceLineTest :: Test
 parseSourceLineTest = TestCase (do
-  assertEqual "" (parseSourceLine "") (SourceTextLine "")
-  assertEqual "" (parseSourceLine "abc") (SourceTextLine "abc")
-  assertEqual "" (parseSourceLine "Hello, world!") (SourceTextLine "Hello, world!"))
+  -- Source line cases.
+  assertEqual "" (SourceTextLine "") (parseSourceLine "")
+  assertEqual "" (SourceTextLine "abc") (parseSourceLine "abc")
+  assertEqual "" (SourceTextLine "Hello, world!") (parseSourceLine "Hello, world!")
+  -- loom:start cases
+  assertEqual "" (SourceTagLine (FragmentStartTag ("test"))) (parseSourceLine "loom:start(test)")
+  -- loom:end cases
+  assertEqual "" (SourceTagLine (FragmentEndTag "test")) (parseSourceLine "loom:end(test)"))
+  
 
 trivialTest :: Test
 trivialTest = TestCase (assertEqual "1 + 1 = 2" (1 + 1) (2 :: Int))
