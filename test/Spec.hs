f23733692ea1881e6dfe06dc80393365caa3c386
Fernando Borretti <fernando@borretti.me>
2023-01-28 19:27:37 +1100

    Add a test of nameChar

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,21 @@
 module Main (main) where
 import Test.HUnit
 import Text.Parsec (parse)
+import AirLoom.Parser (nameChar)
+
+nameCharTest :: Test
+nameCharTest = TestCase (do
+  assertEqual "nameChar" (parse nameChar "" "a") (Right 'a')
+  assertEqual "nameChar" (parse nameChar "" " ") (Right ' ')
+  assertEqual "nameChar" (parse nameChar "" "1") (Right '1'))
 
 trivialTest :: Test
 trivialTest = TestCase (assertEqual "1 + 1 = 2" (1 + 1) (2 :: Int))
 
 tests :: Test
 tests = TestList [
-    TestLabel "trivial" trivialTest
+    TestLabel "trivial" trivialTest,
+    TestLabel "nameChar" nameCharTest
   ]
 
 
