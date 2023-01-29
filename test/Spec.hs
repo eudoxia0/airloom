07e3909d4e82692ea236831264263db104265eac
Fernando Borretti <fernando@borretti.me>
2023-01-29 13:14:02 +1100

    Add ChatGPT-written unit tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 import Test.HUnit
-import AirLoom.Parser (parseSourceLine, SourceLine (SourceTextLine, SourceTagLine), SourceTag (FragmentStartTag, FragmentEndTag))
+import AirLoom.Parser (parseSourceLine, SourceLine (SourceTextLine, SourceTagLine), SourceTag (FragmentStartTag, FragmentEndTag), parseSourceFile)
 
 -- Parser tests.
 
@@ -17,7 +17,15 @@ parseSourceLineTest = TestCase (do
   -- loom:end cases
   assertEqual "" (SourceTagLine (FragmentEndTag "c")) (parseSourceLine "loom:end(c)")
   assertEqual "" (SourceTagLine (FragmentEndTag "d")) (parseSourceLine "    loom:end(d)   "))
-  
+
+parseSourceFileTest :: Test
+parseSourceFileTest = TestCase (do
+    assertEqual "empty file" [] (parseSourceFile "")
+    assertEqual "one text line" [SourceTextLine "abc"] (parseSourceFile "abc")
+    assertEqual "two text lines" [SourceTextLine "abc", SourceTextLine "def"] (parseSourceFile "abc\ndef")
+    assertEqual "text line and start tag" [SourceTextLine "abc", SourceTagLine (FragmentStartTag "foo")] (parseSourceFile "abc\nloom:start(foo)")
+    assertEqual "start tag and end tag" [SourceTagLine (FragmentStartTag "bar"), SourceTagLine (FragmentEndTag "bar")] (parseSourceFile "loom:start(bar)\nloom:end(bar)")
+    assertEqual "text lines, start and end tags" [SourceTextLine "abc", SourceTagLine (FragmentStartTag "foo"), SourceTextLine "def", SourceTagLine (FragmentEndTag "foo"), SourceTextLine "ghi"] (parseSourceFile "abc\nloom:start(foo)\ndef\nloom:end(foo)\nghi"))
 
 trivialTest :: Test
 trivialTest = TestCase (assertEqual "1 + 1 = 2" (1 + 1) (2 :: Int))
@@ -25,7 +33,8 @@ trivialTest = TestCase (assertEqual "1 + 1 = 2" (1 + 1) (2 :: Int))
 tests :: Test
 tests = TestList [
     TestLabel "trivial" trivialTest,
-    TestLabel "parseSourceLine" parseSourceLineTest
+    TestLabel "parseSourceLine" parseSourceLineTest,
+    TestLabel "parseSourceFile" parseSourceFileTest
   ]
 
 
