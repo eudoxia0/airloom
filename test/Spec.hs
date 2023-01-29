b19fe14884d64346bb7c35479276a0e971ab41b9
Fernando Borretti <fernando@borretti.me>
2023-01-29 16:35:05 +1100

    Add tests of JSON serialization and deserialization

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,11 +3,13 @@ module Main (main) where
 import Test.HUnit
 import qualified TestLift
 import qualified TestParser
+import qualified TestStore
 
 tests :: Test
 tests =
   TestList
     [ TestLabel "Parser Tests" TestParser.suite,
+      TestLabel "Store Tests" TestStore.suite,
       TestLabel "Lifting Tests" TestLift.suite
     ]
 
---
+++ test/TestStore.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+module TestStore (suite) where
+
+import AirLoom.Store (Store (Store))
+import Data.Aeson (encode, eitherDecode)
+import qualified Data.HashMap.Strict as Map
+import Data.Maybe (fromJust)
+import Test.HUnit
+
+toJsonTest :: Test
+toJsonTest =
+  TestCase
+    ( do
+        let store = Store $ Map.fromList [("key1", "value1"), ("key2", "value2")]
+        let expected = "{\"key1\":\"value1\",\"key2\":\"value2\"}"
+        let encoded = encode store
+        assertEqual "Serializing Store to JSON" expected encoded
+    )
+
+fromJsonTest :: Test
+fromJsonTest =
+  TestCase
+    ( do
+        let json = "{\"key1\":\"value1\",\"key2\":\"value2\"}"
+        let expected = Store (Map.fromList [("key1", "value1"), ("key2", "value2")])
+        let result = eitherDecode json :: Either String Store
+        assertEqual "Parsing store from JSON" (Right expected) result
+    )
+
+suite :: Test
+suite =
+  TestList
+    [ TestLabel "Serialize to JSON" toJsonTest,
+      TestLabel "Deserialize from JSON" fromJsonTest
+    ]
