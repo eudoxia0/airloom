27507ae8f7ab7807d591c813408110d4def486a0
Fernando Borretti <fernando@borretti.me>
2023-02-11 12:11:30 +1100

    trimPrefixes

--- src/AirLoom/Weave.hs
+++ src/AirLoom/Weave.hs
@@ -5,6 +5,7 @@ import AirLoom.Parser
     DocTag (TranscludeTag),
   )
 import AirLoom.Store (FragmentName, Store, get)
+import Data.Char (isSpace)
 
 data WeaveError = UnknownFragmentError FragmentName
   deriving (Eq, Show)
@@ -16,5 +17,11 @@ weaveLine :: DocLine -> Store -> Either WeaveError String
 weaveLine (DocTextLine s) _ = Right s
 weaveLine (DocTagLine (TranscludeTag name)) store =
   case get store name of
-    Just contents -> Right contents
+    Just contents -> Right $ trimPrefixes contents
     Nothing -> Left (UnknownFragmentError name)
+
+trimPrefixes :: String -> String
+trimPrefixes s = unlines . map (drop commonIndent) . lines $ s
+  where
+    indents = map (length . takeWhile isSpace) . lines $ s
+    commonIndent = minimum indents
